9de504da19e7c6d5571054d882aa7493cf1d951c
user <dmitry.kaverznev@gmail.com>
2025-06-02 10:55:59 +0000

    code: Делаю распознавание

--- app/src/main/java/com/dkaverznev/carcamera/data/ScanRepository.java
+++ app/src/main/java/com/dkaverznev/carcamera/data/ScanRepository.java
@@ -1,29 +1,44 @@
 package com.dkaverznev.carcamera.data;
 
-import android.content.Context;
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import java.util.Objects;
+
 public class ScanRepository {
 
-    private final Context context;
     private final TextRecognizer textRecognizer;
 
-    public ScanRepository(Context context) {
-        this.context = context;
+    private final MutableLiveData<String> _scannedText = new MutableLiveData<>();
+    public final MutableLiveData<String> scannedText = _scannedText;
+
+    private final MutableLiveData<String> _scanErrorMessage = new MutableLiveData<>();
+    public final MutableLiveData<String> scanErrorMessage = _scanErrorMessage;
+
+    public ScanRepository() {
         textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
     }
 
     public void scan(InputImage image) {
         Log.d("ScanRepository", "Запуск распознавания текста в ScanRepository.");
 
+        _scanErrorMessage.setValue(null);
+        _scannedText.setValue(null);
+
         textRecognizer.process(image)
                 .addOnSuccessListener(text -> {
-                    String resultText = text.getText();
+                    _scannedText.setValue(text.getText());
+                    Log.d("ScanRepository", "Текст распознан: " + text.getText());
+                })
+                .addOnFailureListener(e -> {
+                    _scanErrorMessage.setValue(Objects.requireNonNull(e.getMessage()));
+                    Log.e("ScanRepository", "Ошибка распознавания текста: " + e.getMessage(), e);
+                    _scannedText.setValue(null);
                 });
     }
 
--- app/src/main/java/com/dkaverznev/carcamera/viewmodel/ScanViewModel.java
+++ app/src/main/java/com/dkaverznev/carcamera/viewmodel/ScanViewModel.java
@@ -1,32 +1,35 @@
 package com.dkaverznev.carcamera.viewmodel;
 
 import android.app.Application;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-
 import com.dkaverznev.carcamera.data.ScanRepository;
-import com.google.mlkit.vision.common.InputImage; // Импорт InputImage
+import com.google.mlkit.vision.common.InputImage;
 
 public class ScanViewModel extends AndroidViewModel {
 
     private final ScanRepository scanRepository;
 
-    private final MutableLiveData<Boolean> _scanSuccess = new MutableLiveData<>();
-    public LiveData<Boolean> scanSuccess = _scanSuccess;
-
-    private final MutableLiveData<String> _errorMessage = new MutableLiveData<>();
-    public LiveData<String> errorMessage = _errorMessage;
+    public final LiveData<String> scannedText;
+    public final LiveData<String> scanErrorMessage;
 
     public ScanViewModel(@NonNull Application application) {
         super(application);
-        scanRepository = new ScanRepository(application.getApplicationContext());
+        scanRepository = new ScanRepository();
+
+
+        this.scannedText = scanRepository.scannedText;
+        this.scanErrorMessage = scanRepository.scanErrorMessage;
     }
 
-    public void scan(InputImage image) {
+    public void startTextScan(InputImage image) {
         scanRepository.scan(image);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        scanRepository.close();
     }
 }
