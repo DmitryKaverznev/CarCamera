b6ba1f074086a7d8e5882eb7dfe5b7b0d6a2e61b
user <dmitry.kaverznev@gmail.com>
2025-06-03 10:13:03 +0000

    code: Делаю распознавание

--- app/src/main/java/com/dkaverznev/carcamera/viewmodel/ScanViewModel.java
+++ app/src/main/java/com/dkaverznev/carcamera/viewmodel/ScanViewModel.java
@@ -6,6 +6,7 @@ import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import com.dkaverznev.carcamera.data.ScanRepository;
 import com.google.mlkit.vision.common.InputImage;
+import androidx.camera.core.ImageProxy; // Импортируйте ImageProxy
 
 public class ScanViewModel extends AndroidViewModel {
 
@@ -18,13 +19,12 @@ public class ScanViewModel extends AndroidViewModel {
         super(application);
         scanRepository = new ScanRepository();
 
-
         this.scannedText = scanRepository.scannedText;
         this.scanErrorMessage = scanRepository.scanErrorMessage;
     }
 
-    public void startTextScan(InputImage image) {
-        scanRepository.scan(image);
+    public void startTextScan(InputImage image, ImageProxy imageProxy) {
+        scanRepository.scan(image, imageProxy);
     }
 
     @Override
